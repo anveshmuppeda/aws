dbb1e760c4b1c3d8b009dd5c109f05d9be4350c3
Anvesh Muppeda <muppedaanvesh@gmail.com>
2024-01-06 18:47:54 -0600

    updating the serverless notes

--- serverless/aws-node-project/index.js
+++ serverless/aws-node-project/index.js
@@ -3,7 +3,7 @@ module.exports.handler = async (event) => {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: 'Go Serverless v3.0! Your function executed successfully!',
+        message: 'Testing from local test func!',
         input: event,
       },
       null,
