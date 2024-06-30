b97ef2cf4dd5cd164d294f9c99aaccdddaaad2e1
Anvesh Muppeda <muppedaanvesh@gmail.com>
2024-06-30 15:21:30 -0500

    Adding first cdk project

---
+++ cdk/my-first-cdk/test/my-first-cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as MyFirstCdk from '../lib/my-first-cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/my-first-cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new MyFirstCdk.MyFirstCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
